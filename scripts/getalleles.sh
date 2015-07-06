acd30f0666b328669c2474654d87054b45c5dfd3
jpgerke <justin.gerke@gmail.com>
2015-07-06 11:11:26 -0700

    added script to pull alleles from vcf

---
+++ scripts/getalleles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -l
+
+#SBATCH -J getnam
+#SBATCH -e ../logs/getnam-%j-log.err
+#SBATCH -o ../logs/getnam-%j-log.out
+
+zcat /group/jrigrp4/Justin_Kate/GBS2.7/sorted_NAM_children.vcf.gz | awk '{print $3, $4, $5}' > ../alleles.txt
