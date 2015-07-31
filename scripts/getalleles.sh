68c775004a52556988aa0e1adcc54db7f1d5546d
jpgerke <justin.gerke@gmail.com>
2015-07-31 13:36:40 -0700

    updated getalleles.sh to write to correct directory

--- scripts/getalleles.sh
+++ scripts/getalleles.sh
@@ -4,4 +4,4 @@
 #SBATCH -e ../logs/getnam-%j-log.err
 #SBATCH -o ../logs/getnam-%j-log.out
 
-zcat /group/jrigrp4/Justin_Kate/GBS2.7/sorted_NAM_children.vcf.gz | awk '{print $3, $4, $5}' > ../alleles.txt
+zcat /group/jrigrp4/Justin_Kate/GBS2.7/sorted_NAM_children.vcf.gz | awk '{print $3, $4, $5}' > ../data/Namfreqs/alleles.txt
