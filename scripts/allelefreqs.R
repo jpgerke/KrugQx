2ff1a3782bd6cb7047e79db4800ae20588c333cd
jpgerke <justin.gerke@gmail.com>
2015-08-14 17:20:34 -0500

    updated allelefreqs.R to just print out the SNP names

--- scripts/allelefreqs.R
+++ scripts/allelefreqs.R
@@ -7,26 +7,32 @@ load_obj <- function (f) {
   return(env[[nm]])
 }
 
-allelefreqs <- function (filename) {
-  myobj <- load_obj(filename)
-  return(myobj)
-}
-
-getfreq <- function (geno) {
-	return(sum(geno) / (length(geno) *2 ))
-}
-
-calc_freqs <- function (filename) {
-
-	alldata <- load_obj(filename)
-	mycols <- alldata[,-1]
-	freqs <- apply(mycols, 2, getfreq)
-	return(freqs)
-}
+# allelefreqs <- function (filename) {
+#   myobj <- load_obj(filename)
+#   return(myobj)
+# }
+# 
+# getfreq <- function (geno) {
+# 	return(sum(geno) / (length(geno) *2 ))
+# }
+# 
+# calc_freqs <- function (filename) {
+# 
+# 	alldata <- load_obj(filename)
+# 	mycols <- alldata[,-1]
+# 	freqs <- apply(mycols, 2, getfreq)
+# 	return(freqs)
+# }
 
 #running into memory problems running the whole thing so loop by chromosome
+allnames = list()
 for (x in 1:10) {
 	filename <- paste("../data/Namfreqs/cut2uniqueAD_NAM_KIDS_chr", x, ".raw.RData", sep='')
-	freqs <- calc_freqs(filename)
-	write.table(freqs, file=paste("../data/Namfreqs/frequencies_chr", x, ".csv", sep=''), col.names=F, quote=F, sep=',')
+	myobj <- load_obj(filename)
+  mynames = names(myobj)
+  allnames[[x]] = mynames
 }
+rm(myobj)
+rm(mynames)
+fullnames = do.call(c, allnames)
+write(fullnames, file = "../data/Namfreqs/GWAS_SNPs.txt")
