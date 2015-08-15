deb1d92063172c05c79f43ff33d2058f9aa66095
jpgerke <justin.gerke@gmail.com>
2015-08-15 14:23:58 -0500

    check_nam_alleles no builds the dict of frequencies

--- scripts/getalleles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-
-#SBATCH -J getnam
-#SBATCH -e ../logs/getnam-%j-log.err
-#SBATCH -o ../logs/getnam-%j-log.out
-
-zcat /group/jrigrp4/Justin_Kate/GBS2.7/sorted_NAM_children.vcf.gz | awk '{print $3, $4, $5}' > ../data/Namfreqs/alleles.txt
