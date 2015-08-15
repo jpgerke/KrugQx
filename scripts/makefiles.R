761c8b624760b63a8e54371c736357759e3c2ec3
jpgerke <justin.gerke@gmail.com>
2015-08-15 17:05:45 -0500

    first draft of code to make the infiles

--- scripts/makefiles.R
+++ scripts/makefiles.R
@@ -3,6 +3,9 @@ require(dplyr)
 library(reshape2)
 rm(list=ls())
 
+#Take infiles of effects and allele frequencies, and make the necessary files for qx
+#There is definitely some ugly code duplication in here, but it works.
+
 #load the phenotype data
 load("../data/1kweight/SNP_effects_ordered.RData")
 str(df)
@@ -22,20 +25,27 @@ with_ID <- rowwise(df) %>%
                     Allele = str_split(term, pattern = "_")[[1]][3])
 
 ##### match.pop.file
+#all null snps in the gwas pop
 #Format: SNP, CLST, A1, A2, FRQ, POS, CHR, etc.
-match_pop_file <- anti_join(gwas_genome_file, with_ID, by="SNP")
+match_pop_file <- anti_join(gwas_genome_file, with_ID, by="SNP") %>%
+                    rowwise() %>%  #make the SNP ID
+                    mutate(CHR = str_split(SNP, pattern = "_")[[1]][1], POS = str_split(SNP, pattern = "_")[[1]][2]) %>%
+                    rowwise() %>%   #use the SNP ID to make the POS and CHROM columns
+                    mutate(CHR = str_replace(CHR, pattern='S', replacement = '')[1], CLST="NAM") %>%
+                    select(SNP,CLST,A1,A2,FRQ,POS,CHR)
 
 #once ready we will want to make note of which allele is the reference allele
 
-#this will be the full.dataset.file
+#####full.dataset.file
+#all null snps in the test pops
 full_dataset_file_shortform <- anti_join(pop_genome_file, with_ID, by="SNP")
 longform <- melt(full_dataset_file_shortform, 
                  measure.vars = c("KC0Allele1", "KLSAllele1", "KSSAllele1"), 
                  variable.name = "CLST", 
                  value.name = "FRQ") %>%
-            rowwise() %>%
+            rowwise() %>% #make the SNP ID
             mutate(CHR = str_split(SNP, pattern = "_")[[1]][1], POS = str_split(SNP, pattern = "_")[[1]][2]) %>%
-            rowwise() %>%
+            rowwise() %>% #use the SNP ID to make the POS and CHROM columns
             mutate(CHR = str_replace(CHR, pattern='S', replacement = '')[1]) %>%
             select(SNP,CLST,A1,A2,FRQ,POS,CHR)
 
@@ -59,3 +69,9 @@ freqs_file <- inner_join(with_ID, pop_genome_file, by="SNP") %>%
   rowwise() %>%
   mutate(CHR = str_replace(CHR, pattern='S', replacement = '')[1]) %>%
   select(SNP, CLST, A1, A2, FRQ, POS, CHR)
+
+
+write.csv(freqs_file, file = "../qxfiles/1kwt/freqfile2.csv", quote=F, row.names = F)
+write.csv(gwas_data_file_final, file = "../qxfiles/1kwt/gwas_file2.csv", quote=F, row.names = F)
+write.csv(longform, file = "../qxfiles/1kwt/full_dataset_file2.csv", quote=F, row.names = F)
+write.csv(match_pop_file, file = "../qxfiles/1kwt/match_pop_file2.csv", quote=F, row.names=F)
