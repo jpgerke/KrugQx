e524336998d03d72fc4ad6b2e3268a6655d47a15
jpgerke <justin.gerke@gmail.com>
2015-08-15 15:30:57 -0500

    built the NAM genome file

--- scripts/build_namalleles.py
+++ scripts/build_namalleles.py
@@ -1,20 +1,35 @@
 
 import pickle as pkl
 
-infile = open("../data/Namfreqs/alleles.txt", 'r')
+freqs = pkl.load(open("../data/Namfreqs/namfreqdict.pkl", 'rb'))
 
-alleledict = {}
+snpfile = open("../data/Namfreqs/GWAS_SNPS.txt", 'r')
 
-#skip the first line
-infile.readline()
+outfile = open('nam_genome_file.csv', 'w')
+outfile.write("SNP,A1,A2,FRQ\n")
 
-for line in infile:
-	entry = line.rstrip().split()
-	#format will be major, minor
-	alleledict[entry[0]] = (entry[1], entry[2])
+count = 0
+found = 0
+for line in snpfile:
+	entry = line.rstrip().split('_')
+	if entry[0] == "FID":
+		continue
+	count += 1
+	SNP = entry[0] + "_" + entry[1]
+	#allele 1 needs to be the allele the effect was calculated for
+	myallele = entry[2]
+	if SNP in freqs:
+		myfreq = freqs[SNP][myallele]
+		alleles = freqs[SNP].keys()
+		if alleles[0] == myallele:
+			A1 = alleles[0]
+			A2 = alleles[1]
+		else:
+			assert alleles[1] == myallele
+			A1 = alleles[1]
+			A2 = alleles[0]
+	#	print SNP, freqs[SNP], myfreq, myallele, A1, A2
+		found += 1
+		outfile.write(','.join(map(str, [SNP, A1, A2, myfreq])) + '\n')
 
-#for key,value in alleledict.iteritems():
-#	print key, value
-#	break
-
-pkl.dump(alleledict, open("../data/Namfreqs/alleledict.pkl", 'wb'))
+print count, found
