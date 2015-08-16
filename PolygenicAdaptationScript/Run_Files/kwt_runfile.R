aabef7aa07c9a299955cd5c1073697d0b115c4ec
jpgerke <justin.gerke@gmail.com>
2015-08-15 21:58:16 -0500

    ran qx on kwt

--- PolygenicAdaptationScript/Run_Files/kwt_runfile.R
+++ PolygenicAdaptationScript/Run_Files/kwt_runfile.R
@@ -1,14 +1,15 @@
 rm(list=ls())
-source("../Scripts/functions_modified.R")
+setwd("../")
+source("./Scripts/functions_modified.R")
 
-PolygenicAdaptationFunction ( 
-  gwas.data.file = "../../qxfiles/1kwt/gwas_file2.csv", 
-  freqs.file = "../../qxfiles/1kwt/freqfile2.csv" , 
-  env.var.data.files = list ("../EnvVar/Environment.txt" ) , 
+kwt <- PolygenicAdaptationFunction ( 
+  gwas.data.file = "../qxfiles/1kwt/gwas_file2.csv", 
+  freqs.file = "../qxfiles/1kwt/freqfile2.csv" , 
+  env.var.data.files = list ("./EnvVar/Environment.txt" ) , 
   # Note: you can supply as many env.var.data.files concurrently as you want. If only supplying one file it should still be included in a list, e.g. env.var.data.files = list ( "Example/EnvVar/HGDP_LATS_GLOBAL")
-  match.pop.file = "../../qxfiles/1kwt/match_pop_file2.csv",
-  full.dataset.file = "../../qxfiles/1kwt/full_dataset_file2.csv" , 
-  path = "../../qxfiles/1kwt/output/" , 
+  match.pop.file = "../qxfiles/1kwt/match_pop_file2.csv",
+  full.dataset.file = "../qxfiles/1kwt/full_dataset_file2.csv" , 
+  path = "../qxfiles/1kwt/output/" , 
   match.categories = c ("FRQ") ,
   match.bins = list ( seq ( 0 , 1, 0.5 )) ,
   cov.SNPs.per.cycle = 5000 , 
@@ -16,39 +17,17 @@ PolygenicAdaptationFunction (
   null.phenos.per.cycle = 1000 , 
   null.cycles = 1 ,
   load.cov.mat = F ,
-  sim.null = T ,
+  sim.null = F ,
   check.allele.orientation = T
 )
 ######################### DO ANALYSIS FOR GWAS P-VALUES < 1e-4 #########################
-Out <- list()
-for(i in 1:22){
-traitFile = paste("Trait_Data/",dir("Trait_Data")[i+22],sep="")
-freqsFile = paste("Trait_Data/",dir("Trait_Data")[i],sep="")
-pathDir = strsplit(traitFile,split="[.]")[[1]][3]
-
-Out[[i]] <- PolygenicAdaptationFunction (	gwas.data.file = traitFile,
-				freqs.file = freqsFile ,
-				env.var.data.files = list ( "EnvVar/Environment.txt") ,
-							match.pop.file = "Genome_Data/match.pop.file.txt" ,
-							full.dataset.file = "Genome_Data/full.dataset.file.txt" ,
-							path = paste("OUTPUT/",pathDir,sep="") ,
-							match.categories = c ( "FRQ") ,
-							match.bins = list ( seq ( 0 , 1 , 0.02 ) ) ,
-							cov.SNPs.per.cycle = 5000 ,
-							cov.cycles = 1 ,
-							null.phenos.per.cycle = 1000 ,
-							null.cycles = 10 ,
-							load.cov.mat = T ,
-							sim.null = F ,
-							check.allele.orientation = F
-							)
 
 
 ### Make table of asymptotic Qx p-values
-QxPvalues <- matrix(NA,ncol=6,nrow=22)
+QxPvalues <- matrix(NA,ncol=5,nrow=22)
 QxPvalues <- data.frame(QxPvalues)
 names(QxPvalues) <- c("Phenotype","Overdispersion-P","MH-P","ML-P","SAH-P","SAL-P")
-for(i in 1:22){
+for(i in 1){
 QxPvalues[i,1] <- names(Out)[i]
 QxPvalues[i,2] <- Out[[i]]$asymptotic.p.vals$Qx
 QxPvalues[i,3:6] <- Out[[i]]$asymptotic.p.vals$ind.Z
